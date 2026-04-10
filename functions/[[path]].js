ae2ffd24accab7cc8c68e6bdb3b0d3cf21577bd0
Eeshan <eeshan@gmail.com>
2026-04-10 22:14:34 +0100

    Add Backtrack to list of projects

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -8,7 +8,8 @@ export async function onRequest(context) {
     'vibewho': 'vibewho.pages.dev',
     'startups-against-humanity': 'startups-against-humanity.pages.dev',
     'letters-practice': 'letters-practice.pages.dev',
-    'things-in-rings': 'things-in-rings.pages.dev'
+    'things-in-rings': 'things-in-rings.pages.dev',
+    'backtrack': 'backtrack-61p.pages.dev'
   };
 
   if (projectSlug && projectMap[projectSlug]) {
